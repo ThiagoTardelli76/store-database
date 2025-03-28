be8b78692eaf0949a673a33a58760b20e36f3dbf
Thiago Tardelli <thiagostardelli@gmail.com>
2025-03-28 09:27:29 +0000

    sixth commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Habilita exportação estática
+  images: {
+    unoptimized: true, // Evita problemas com imagens no GitHub Pages
+  },
+  basePath: "/thiagotardelli76.github.io", // Substitua pelo nome exato do seu repositório
+  assetPrefix: "/thiagotardelli76.github.io",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
