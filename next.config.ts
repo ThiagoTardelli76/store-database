ab5564eb7a195d42604ac30d281bbda216050e40
Thiago Tardelli <thiagostardelli@gmail.com>
2025-03-28 10:00:58 +0000

    seventh commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Habilita exportação estática
-  images: {
-    unoptimized: true, // Evita problemas com imagens no GitHub Pages
-  },
-  basePath: "/thiagotardelli76.github.io", // Substitua pelo nome exato do seu repositório
-  assetPrefix: "/thiagotardelli76.github.io",
+  output: "export",
+  images: { unoptimized: true },
+
 };
 
 module.exports = nextConfig;
